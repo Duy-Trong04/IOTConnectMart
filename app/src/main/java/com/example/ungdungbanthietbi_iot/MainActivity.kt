c332452860b8bce4a1234c10ce4a7f157f3630fb
0306221306namcao <0306221306@caothang.edu.vn>
2024-12-09 12:38:13 +0700

    commit

--- app/src/main/java/com/example/ungdungbanthietbi_iot/MainActivity.kt
+++ app/src/main/java/com/example/ungdungbanthietbi_iot/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UngDungBanThietBi_IOTTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UngDungBanThietBi_IOTTheme {
-        Greeting("Android")
-    }
-}
